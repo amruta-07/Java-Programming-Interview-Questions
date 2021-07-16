c2a4362c12e30b95ae7e561a11a2a186c2c89f16
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-07-16 16:56:46 +0530

    Add files via upload

---
+++ CountNumberOfDigits.java
@@ -0,0 +1,16 @@
+public class CountNumberOfDigits{
+    public static void main(String[] args){
+
+        int num=123456;
+        int count=0;
+        while(num>0)
+        {
+          num=num/10;
+          count++;
+        }
+
+       System.out.println("Number of digits:" +count);
+
+  }
+  
+}
