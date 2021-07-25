24ef647b9caa7552ccad9babf91127ecbc8caf2d
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-07-25 22:02:31 +0530

    Add files via upload

---
+++ tut1.java
@@ -0,0 +1,22 @@
+class tut1
+{
+   public static void main(String args [])
+{     
+      int[][] x= new int[2][3];
+      System.out.println(x);
+      System.out.println(x[0]);
+      System.out.println(x[0][0]);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+}
