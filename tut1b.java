bb304dd327a1d53b6a05a1a102640102c4de14f1
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-07-26 22:33:59 +0530

    Add files via upload

---
+++ tut1b.java
@@ -0,0 +1,20 @@
+class tut1b
+{
+  public static void main(String [] args)
+ {  
+   int i=0;
+   for(int j=0; j<3; j++)
+   {
+     i=i+j;
+   }
+   System.out.println(i +"..."+ j);
+  }
+
+
+
+
+
+
+
+
+}
