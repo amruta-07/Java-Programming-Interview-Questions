cb08f87d72ad70c2450fd613f91ea0fcd7fd40db
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-08-05 00:04:13 +0530

    Add files via upload

---
+++ StarPattern8.java
@@ -0,0 +1,30 @@
+class StarPattern8{
+  public static void main(String [] args){
+
+   for(int i=1;i<=4;i++)
+   {
+      for(int j=1;j<=i;j++)
+        {
+          System.out.print(" ");
+        }
+      for(int k=4;k>=i;k--)
+        {
+          System.out.print("*");
+        }
+      for(int l=3;l>=i;l--)
+        {
+          System.out.print("*");
+        }
+         System.out.println();
+    
+
+   }
+
+ }
+
+}
+/*output:     *******
+               *****
+                ***
+                 *     */
+       
