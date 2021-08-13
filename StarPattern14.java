8d5351d2f1449a0ff60999e3038ebf19d9554755
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-08-13 08:31:51 +0530

    Add files via upload

---
+++ StarPattern14.java
@@ -0,0 +1,27 @@
+class StarPattern14{
+   public static void main(String [] args){
+     for(int i=1;i<=5;i++)
+     {
+        for(int j=1;j<=5;j++)
+          {
+              if(i>=2 && j>=2 && i<=4 && j<=4)
+               {
+                 System.out.print(" ");
+               }
+               else
+               {
+                 System.out.print("*");
+               }
+          }
+           System.out.println();
+      }
+
+  }
+
+
+}
+/* output:   *****
+             *   *
+             *   *
+             *   *
+             *****     */
