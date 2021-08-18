12f86330830b02df1ca558d63acf5925f44ee0d1
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-08-18 23:57:01 +0530

    Add files via upload

---
+++ NumericPattern3.java
@@ -0,0 +1,31 @@
+class NumericPattern3
+{
+    public static void main(String args[])
+     {
+        int count=0;
+         for(int i=1;i<=4;i++)
+         {
+           for(int j=1;j<=i;j++)
+            {
+               count=count+1;
+               System.out.print(count);
+             }
+             System.out.println();
+
+
+
+          }
+
+      }
+
+
+
+}
+
+/*output:
+
+1
+23
+456
+78910   */
+         
