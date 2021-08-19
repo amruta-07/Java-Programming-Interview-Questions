ab6a92f3a6f0ae6b7ee57b979af6cc4369667ad6
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-08-19 23:08:30 +0530

    Add files via upload

---
+++ NumericPattern4.java
@@ -0,0 +1,29 @@
+class NumericPattern4
+{
+  public static void main(String [] args)
+   {
+     for(int i=1;i<=5;i++)
+     {
+       for(int j=i;j>=1;j--)
+         {
+            System.out.print(j+" ");
+
+         }
+          System.out.println();
+     }
+
+
+   }
+
+}
+
+
+
+/* output
+
+1
+2 1
+3 2 1
+4 3 2 1 
+5 4 3 2 1     */
+
